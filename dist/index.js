d105275e2509bb6872d8358db09f523e908c666e
onurravli <onurravli@hotmail.com>
2023-01-20 12:23:15 +0300

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.validate = void 0;
+const validate = (id) => {
+    return false;
+};
+exports.validate = validate;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const validate = (id: number): boolean => {
+	return false;
+};
